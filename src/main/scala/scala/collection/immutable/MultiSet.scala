791f7999722ce7b658741a0cae778f8afb6d2378
marklemay <riteofwhey@gmail.com>
2017-04-14 23:10:26 -0400

    (hack) limit the number of nodes redundant nodes considered

--- src/main/scala/scala/collection/immutable/MultiSet.scala
+++ src/main/scala/scala/collection/immutable/MultiSet.scala
@@ -2,10 +2,22 @@ package scala.collection.immutable
 
 import scala.collection.generic.CanBuildFrom
 import scala.collection.AbstractIterator
+import scala.util.hashing.Hashing
+import scala.collection.immutable.LimitBags.LimitingConfiguration
 
 object MultiSet {
   //Things to make Bags useful
-  implicit def defualtConfig[A] = Bag.configuration.compact[A]
+  //  implicit def defualtConfig[A] = Bag.configuration.compact[A]
+
+  //TODO: this is a bad place to put this configuration
+  implicit def bagMax[A] = LimitBags.LimitBags[A](10)
+
+  implicit def defualtConfig[A] = {
+
+    //    println("HI!!!!")
+
+    LimitBags.compactLimited[A] //Bag.configuration.compact[A]
+  }
 
   case class SetExtention[A](val s: Set[A]) {
     def toBag: Bag[A] = {
@@ -42,7 +54,6 @@ object MultiSet {
     //
     //    }
   }
-  //  Bag.from(elemCounts)
 
   implicit def setToSetExtention[A](a: Set[A]) = SetExtention(a)
 
@@ -53,47 +64,78 @@ object MultiSet {
     }
 
   }
-  //  Bag.from(elemCounts)
 
   implicit def listToListExtention[A](a: List[A]) = ListExtention(a)
 
-  //    case class BagExtention[A](val s: Bag[A]) {
-  //    
-  //        def subsets(): Iterator[Bag[A]] = new AbstractIterator[Bag[A]] {
-  //    private val elms = self.toIndexedSeq
-  //    private var len = 0
-  //    private var itr: Iterator[This] = Iterator.empty
+}
+
+object LimitBags {
+
+  //I think these configurtions are weak sauce
+  sealed trait LimitingConfiguration[A] {
+    val limitNum: Int
+
+    def limit(n: Int) = if (n < limitNum) { n } else { limitNum }
+  }
+
+  case class LimitBags[A](override val limitNum: Int) extends LimitingConfiguration[A]
+
+  class MultiplicityBagBucketLimited[A](override val sentinel: A, val mult: Int)(implicit lim: LimitingConfiguration[A])
+      extends MultiplicityBagBucket[A](sentinel, lim.limit(mult)) {
+    require(lim.limitNum >= this.multiplicity)
+
+    override def added(elem: A, count: Int) = {
+      if (count > 0)
+        new MultiplicityBagBucketLimited(sentinel, multiplicity + count)
+      else
+        this
+    }
+
+    override def addedBucket(bucket: collection.BagBucket[A]) = {
+      new MultiplicityBagBucketLimited[A](sentinel, this.multiplicity + bucket.multiplicity(sentinel))
+    }
+
+    override def -(elem: A): MultiplicityBagBucket[A] = {
+      new MultiplicityBagBucketLimited(sentinel, Math.max(0, multiplicity - 1))
+    }
+
+    override def removed(elem: A, count: Int): BagBucket = {
+      new MultiplicityBagBucketLimited(sentinel, Math.max(0, multiplicity - count))
+    }
+
+    override def intersect(that: collection.BagBucket[A]): BagBucket =
+      new MultiplicityBagBucketLimited(sentinel, Math.min(this.multiplicity, that.multiplicity(sentinel)))
+
+    override def diff(that: collection.BagBucket[A]): BagBucket =
+      new MultiplicityBagBucketLimited(sentinel, Math.max(this.multiplicity - that.multiplicity(sentinel), 0))
+
+    override def distinct: BagOfMultiplicitiesBagBucket[A]#BagBucket = {
+      if (multiplicity <= 1) this
+      else new MultiplicityBagBucketLimited(sentinel, 1)
+    }
+  }
+
+  private class HashedMultiplicityBagLimitedConfiguration[A](val equivClass: Equiv[A] with Hashing[A])(implicit lim: LimitingConfiguration[A]) extends HashedBagConfiguration[A] {
+    def empty(sentinel: A): MultiplicityBagBucket[A] = new MultiplicityBagBucketLimited(sentinel, 0)
+  }
+
+  def compactLimited[A](implicit lim: LimitingConfiguration[A]): HashedBagConfiguration[A] = new HashedMultiplicityBagLimitedConfiguration(collection.HashedBagConfiguration.defaultHashedEquiv[A])
+
+  //  
+  //  
+  //  
+  //  import scala.collection.immutable.HashedBagConfiguration
+  //  
+  //  def compact[A]: HashedBagConfiguration[A] = new HashedBagConfiguration.HashedMultiplicityBagConfiguration(collection.HashedBagConfiguration.defaultHashedEquiv[A])
   //
-  //    def hasNext = len <= elms.size || itr.hasNext
-  //    def next = {
-  //      if (!itr.hasNext) {
-  //        if (len > elms.size) Iterator.empty.next()
-  //        else {
-  //          itr = new SubsetsItr(elms, len)
-  //          len += 1
-  //        }
-  //      }
-  //
-  //      itr.next()
-  //    }
+  //  
+  //  
+  //  
+  //  
+  //    private class HashedMultiplicityBagConfiguration[A](val equivClass: Equiv[A] with Hashing[A]) extends immutable.HashedBagConfiguration[A] {
+  //    def empty(sentinel: A): immutable.MultiplicityBagBucket[A] = new immutable.MultiplicityBagBucket(sentinel, 0)
   //  }
-  //    
   //
-  //  implicit def bagToBagExtention[A](bag: Bag[A]) = BagExtention(bag)
+  //  
 
-  //  implicit def canBuildFrom[A]: CanBuildFrom[Bag[_], A, Bag[A]] = ??? ///setCanBuildFrom[A]
-
-  //type testing
-
-  val v = Bag(1, 1, 1, 2, 3, 4, 5, 5).map(_ % 2 == 0) //(scala.collection.immutable.MultiSet.canBuildFrom)
-
-  //  case class IteratorExtention[A](val s: Set[A]) {
-  //    def toBag: Bag[A] = {
-  //      val counts = s.map((_, 1)).toSeq
-  //      Bag.from(counts: _*)
-  //    }
-  //  }
-  //  //  Bag.from(elemCounts)
-  //
-  //  implicit def setToSetExtention[A](a:Set[A]) = SetExtention(a)
 }
